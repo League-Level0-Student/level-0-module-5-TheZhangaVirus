a361de02b38264affc5679586c098e980be94cb1
League Student <league@iMac-13.attlocal.net>
2019-07-09 18:26:55 -0700

    7/9/2019
    :smile:

--- src/_01_prime_or_not/primeOrNot.java
+++ src/_01_prime_or_not/primeOrNot.java
@@ -1,5 +1,27 @@
 package _01_prime_or_not;
 
-public class primeOrNot {
+import javax.swing.JOptionPane;
 
+public class primeOrNot {
+	
+	public static void main(String[] args) {
+		
+		String number = JOptionPane.showInputDialog("enter a number");
+		int Number = Integer.parseInt(number); {
+		
+			boolean prime = false;
+			
+					for(int i = Number-1; i > 1; i--) {
+					if(Number % i != 0) {
+						prime = true;
+					} else if(Number % i == 0) {
+						prime = false;
+						System.out.println("composite");
+						System.exit(0);
+					}
+
+				}
+					System.out.println("prime");	
+		}
+	}
 }
