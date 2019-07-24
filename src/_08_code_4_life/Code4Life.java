845b035327d47754b4b8d4e0c630ee317ccf57f5
TheZhangaVirus <49412590+TheZhangaVirus@users.noreply.github.com>
2019-07-23 20:06:55 -0700

    7/23/2019
    :smile:

--- src/_08_code_4_life/Code4Life.java
+++ src/_08_code_4_life/Code4Life.java
@@ -18,32 +18,33 @@ public class Code4Life {
 	// 1. Ask the user how many hours they spent coding this week.
 
 	String hours = JOptionPane.showInputDialog("how many hours did you code this week?");
-	int hour = Integer.parseInt(hours);
+	int hour = Integer.parseInt(hours); {
 	
 	// 2. If it is less than or equal to 2, tell them to stop watching YouTube and
 	// write code instead.
 
-	if(hour <= 2) {
-		JOptionPane.showMessageDialog(null, "stop watching youtube and code instead");
-	} else if(hour <= 5 && hour >= 3) {
-		JOptionPane.showMessageDialog(null, "you are a code ninja");
-	} else if(hour >= 5) {
-		playBatmanTheme();
-	}
-			
+		if(hour <= 2) {
+			JOptionPane.showMessageDialog(null, "stop watching youtube and code instead");
+		} else if(hour <= 5 && hour >= 3) {
+			JOptionPane.showMessageDialog(null, "you are a code ninja");
+		} else if(hour >= 5) {
+			//playBatmanTheme();
+		}
+	}		
 	// 3. If it is greater than or equal to 3 AND less than or equal to 5, tell them
 	// they're a Code Ninja
 
 	// 4. If it is more than 5, call the method below to play the Batman theme song.
 
-	private static void playBatmanTheme() {
-		try {
-			AudioClip sound = JApplet.newAudioClip(Code4Life.class.getResource("batman.wav"));
-			sound.play();
-			Thread.sleep(60002);
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-	}
-
+	/*		private static void playBatmanTheme() {
+	 *			try {
+	 *				AudioClip sound = JApplet.newAudioClip(Code4Life.class.getResource("batman.wav"));
+	 *				sound.play();
+	 *				Thread.sleep(60002);
+	 *			} catch (Exception ex) {
+	 *				ex.printStackTrace();
+	 *			}
+	 *		}
+	 */
+	
 }
